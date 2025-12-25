7a391130de32ec590ef4d9181a16e99e6bc5dbe7
park seung won <f1212park20@gmail.com>
2025-12-25 16:09:01 +0900

    index commit

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 from flask import Flask, render_template, request, redirect
+import pymysql
+
 
 app = Flask(__name__)
 
+def get_connection():
+    connection = pymysql.connect(
+        host='my-mysql',
+        user='flaskuser',
+        password='flaskpass',
+        database='testdb',
+        port=3306,
+        cursorclass=pymysql.cursors.DictCursor  # <-- 여기가 핵심
+    )
+    return connection
+
+
 @app.route("/")
 def home():
-    return render_template("index.html");
+    conn = get_connection()
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT * FROM users;")
+            users = cursor.fetchall()  # 결과 가져오기
+
+    finally:
+        conn.close()
+
+    return render_template("index.html", users=users);
